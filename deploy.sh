c2561dec587ae610fcd1fb0147e4c05b69daffa5
fabcre <fabien@await.fr>
2022-03-08 17:21:03 +0100

    fix: deploy and package json

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env sh
+echo "$1"
 
 # abort on errors
-set -e
+# set -e
 
-npm run docs:build
-
-cd docs/.vitepress/dist
-
+pnpm run clean:dist
+pnpm run docs:build
+cd docs/.vitepress/dist || exit
 git init
+git remote add origin https://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@github.com/FabCre/VitePress-Docs.git
+git fetch --all
+echo "Fetched"
+sleep 5
+echo "Return"
 git add -A
-git commit -m 'deploy: github page'
-
-git push -f git@github.com/FabCre/VitePress-Docs.git main:gh-pages
-
-cd -
+git switch gh-pages
+sleep 5
+git commit -m 'deploy'
+git status
+echo "Checkout gh-pages"
+git status
+git push -f
+echo "Pushed on github"
+$SHELL
